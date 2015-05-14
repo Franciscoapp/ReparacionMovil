c52a89a76ce3ddf101fbdf2ba0093b6861cd6798
JonathanSalazar <jonsalazar99@hotmail.com>
2015-05-14 02:09:04 -0600

    se configuró la base de datos

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
+            'database'  => env('DB_DATABASE', 'yoloarreglocore'),
+            'username'  => env('DB_USERNAME', 'admin'),
+            'password'  => env('DB_PASSWORD', 'webweb'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
