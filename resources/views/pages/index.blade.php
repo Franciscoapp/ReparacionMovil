732e43a62bcad334f92aa6be323da1d949c2b52f
Kamnom <kamnom@gmail.com>
2015-05-18 13:25:24 -0600

    Update index.blade.php
    Se cambio Barra de Navegacion a Master.blade

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -9,33 +9,6 @@
 	<link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
 @stop
 
-@section('barra-logo')
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				</button>
-				<a class="" href="/ReparacionMovil/public/">
-				<img class="img-rounded logo" alt="Brand" src="{{ asset('img/logo.png') }}">
-				</a>
-			</div>
-
-			<div class="collapse navbar-collapse">
-				<ul class="nav navbar-nav navbar-right">
-						<li><a href="#">Sign In</a></li>
-						<li><a href="#">Log In</a></li>
-				</ul>
-			</div>
-
-		</div>
-	</nav>
-@stop
-
 @section('js')
 	<script src="{{ asset('js/jquery-1.11.2.min.js') }}"></script>
 	<script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
