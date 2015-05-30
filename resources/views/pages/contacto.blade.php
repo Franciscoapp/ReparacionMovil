5d60541fd6e91971905dcdaf61cbb40f68d10481
Francisco Peña <franciscoapp92@gmail.com>
2015-05-30 13:50:30 -0600

    Se agrego la seccion de contacto

--- resources/views/pages/contacto.blade.php
+++ resources/views/pages/contacto.blade.php
@@ -4,4 +4,54 @@
 @stop
 
 @section('contenido')
+	<div class="container container-small container-white">
+
+      <div class="info">
+        <h1>Información de contácto</h1>
+        <p class="lead">En esta sección podrá encontrar información que
+			te servirá para ponerte en contacto con nosotros ya sea por
+			teléfono o por redes sociales.
+        </p>
+      </div>
+
+      <div class="row marketing">
+      	<hr>
+        <div class="col-md-5 col-md-offset-1">
+          <h4>Teléfono</h4>
+          <p>Fijo: (667) 1-522-554</p>
+          <p>Celular: (667) 7-522-589</p>
+
+          <h4>Facebook</h4>
+          <p>
+          	<a href="">
+          		https://www.facebook.com/YoLoReparo
+          	</a>
+          </p>
+
+          </div>
+
+        <div class="col-lg-5">
+          <h4>Twitter</h4>
+          <p>
+          	<a href="">
+          		https://twitter.com/YoLoReparo
+          	</a>
+          </p>
+
+          <h4>Correo Electrónico</h4>
+          <p>
+          	<a href="">
+          		Contacto@YoLoReparo.com.mx
+          	</a>
+          </p>
+
+          </div>
+      </div>
+
+      <footer class="footer">
+      	<hr>
+        <p>© Francisco Peña 2015</p>
+      </footer>
+
+    </div> <!-- /container -->
 @stop
