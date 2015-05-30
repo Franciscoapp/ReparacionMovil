a8829ef24db1d9f06f254e2b8f650058c373b431
Francisco Peña <franciscoapp92@gmail.com>
2015-05-30 12:03:09 -0600

    Se agrego segunda barra de navegacion, se corrigio la primera barra para desplegarse en pantallas pequeñas

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -2,35 +2,55 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>@yield('titulo')</title>
-		@yield('css')
+		<title>Reparación Móvil</title>
+		<link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+		<link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
 	</head>
 	<body>
-		@yield('barra-logo')
-		<nav class="navbar navbar-default">
+		<nav class="navbar navbar-default barra-logo" style="margin-bottom: 0px;">
 			<div class="container-fluid">
-			
-			<div class="navbar-header">
-				 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-				 <span class="sr-only">Toggle navigation</span>
-				 <span class="icon-bar"></span>
-				 <span class="icon-bar"></span>
-				 <span class="icon-bar"></span>
-				 </button>
-				 <a class="" href="/ReparacionMovil/public/">
-				 <img class="img-rounded logo" alt="Brand" src="{{ asset('img/logo.png') }}">
-				 </a>
-			</div>
-				
-			<div class="collapse navbar-collapse">
-					<ul class="nav navbar-nav navbar-right">
-					<li><a href="#">Sign In</a></li>
-					<li><a href="#">Log In</a></li>
-					</ul>
-			</div>
+				<!-- Brand and toggle get grouped for better mobile display -->
+			    <div class="navbar-header">
+			      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#barra-logo">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			      </button>
+					 <a class="" href="/ReparacionMovil/public/">
+					 	<img class="img-rounded logo" alt="Brand" src="{{ asset('img/logo.png') }}">
+					 </a>
+				</div>
+				<div class="collapse navbar-collapse" id="barra-logo">
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="">Sign In</a></li>
+							<li><a href="">Log In</a></li>
+						</ul>
+				</div>
 			</div>
 		</nav>
+		<nav class="navbar navbar-inverse">
+			<div class="container-fluid">
+			    <!-- Brand and toggle get grouped for better mobile display -->
+			    <div class="navbar-header">
+			      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#barra-nav">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			      </button>
+			    </div>
 
-		@yield('js')
+			    <!-- Collect the nav links, forms, and other content for toggling -->
+			    <div class="collapse navbar-collapse" id="barra-nav">
+			      <ul class="nav navbar-nav">
+			        <li><a href="">Tutoriales</a></li>
+			        <li><a href="">Acerca</a></li>
+			      </ul>
+		    	</div><!-- /.navbar-collapse -->
+			</div><!-- /.container-fluid -->
+		</nav>
+		<script src="{{ asset('js/jquery-1.11.2.min.js') }}"></script>
+		<script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
 	</body>
 </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,15 +1,5 @@
 @extends('master')
 
 @section('titulo')
-	Reparación Móvil
-@stop
-
-@section('css')
-	<link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
-	<link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
-@stop
-
-@section('js')
-	<script src="{{ asset('js/jquery-1.11.2.min.js') }}"></script>
-	<script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
+	
 @stop
